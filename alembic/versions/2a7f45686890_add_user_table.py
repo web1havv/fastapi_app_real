b92e8b1a4cb3bebe40e765e0733050eba5301a69
Vaibhav <f20212328@pilani.bits-pilani.ac.in>
2025-01-07 19:06:30 +0530

    Alemb done

--- alembic/versions/2a7f45686890_add_user_table.py
+++ alembic/versions/2a7f45686890_add_user_table.py
@@ -17,4 +17,19 @@ down_revision: Union[str, None] = '2fe029f628a9'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
+def upgrade():
+    op.create_table('users',
+                    sa.Column('id', sa.Integer(), nullable=False),
+                    sa.Column('email', sa.String(), nullable=False),
+                    sa.Column('password', sa.String(), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True),
+                              server_default=sa.text('now()'), nullable=False),
+                    sa.PrimaryKeyConstraint('id'),
+                    sa.UniqueConstraint('email')
+                    )
+    pass
 
+
+def downgrade():
+    op.drop_table('users')
+    pass
