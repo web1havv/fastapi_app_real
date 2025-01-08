3353d23d92e34439a24ca76844b9021d92d1bb38
Vaibhav <f20212328@pilani.bits-pilani.ac.in>
2025-01-08 07:03:44 +0530

    BEFORE HEROKU

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI, Depends
 from psycopg2.extras import RealDictCursor  # type: ignore
 from pydantic.v1 import BaseSettings
 from sqlalchemy.orm import Session
+from fastapi.middleware.cors import CORSMiddleware
 
 from . import models
 from .database import engine, get_db
@@ -33,6 +34,15 @@ class Settings(BaseSettings):
 settings = Settings()
 print(settings.database_password)
 app = FastAPI()
+origins = ['*']
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(posts.router)
 app.include_router(users.router)
